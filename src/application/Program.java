1d80a043a48a0c9a746a090cc3887a485d2d7f2e
Thiago Mussolini <mussolinithiago1994@gmail.com>
2022-10-14 06:39:46 -0300

    Finalizado o laço while e o laço for. Finalizando primeira parte do projeto

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,29 +24,46 @@ public class Program {
 		String marca = sc.nextLine();
 		System.out.print("Preco do produto: ");
 		Double preco = sc.nextDouble();
-		System.out.println("Quantidade: ");
+		System.out.print("Quantidade: ");
 		Integer quantidade = sc.nextInt();
 		
 		
 		Produto produto = new Produto(nome, marca, preco, quantidade);
 		list.add(produto);
 		
-		System.out.println("Deseja encerrar a operação? (y/n): ");
+		System.out.print("Deseja encerrar a operação? (y/n): ");
 		char options = sc.next().charAt(0);
+		sc.nextLine();
 		
-		while (options != 'n') {
+		while (options != 'y') {
 			System.out.print("Nome do produto: ");
 			nome = sc.nextLine();
 			System.out.print("Marca do produto: ");
 			marca = sc.nextLine();
 			System.out.print("Preco do produto: ");
 			preco = sc.nextDouble();
-			System.out.println("Quantidade: ");
+			System.out.print("Quantidade: ");
 			quantidade = sc.nextInt();
 			
 			produto = new Produto(nome, marca, preco, quantidade);
 			list.add(produto);
+			System.out.print("Deseja encerrar a operação? (y/n): ");
+			options = sc.next().charAt(0);
+			sc.nextLine();
 		}
+		
+		double total = 0;
+		double valorQuantidade = 0;
+		int i =0;
+		
+		for (Produto prod:list) {
+			System.out.printf(i + " -- " + prod.getNome() + " -- " + prod.getQuantidade() + "un -- R$: %.2f%n", prod.getPreco());
+			System.out.println("------------------------");
+			valorQuantidade = prod.getPreco() * prod.getQuantidade();
+			total += valorQuantidade;
+			i ++;
+		}
+		System.out.printf("Total:      R$: %.2f%n", total);
 		sc.close();
 
 	}
